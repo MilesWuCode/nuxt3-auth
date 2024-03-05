9216e192f5a89ac273167a197482edcf96071d6a
Miles Wu <slion0725@gmail.com>
2024-03-05 11:32:14 +0800

    base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
+
+  eslint: {
+    // 啟動時檢查代碼,建議開
+    lintOnStart: true,
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="text-3xl">Page: index</div>
+</template>
+
+<style scoped></style>
